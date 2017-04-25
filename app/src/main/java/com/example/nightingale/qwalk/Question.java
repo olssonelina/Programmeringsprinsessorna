8658d56ab9223353ae79e3f3f5fe09e18f427840
Elina Olsson <elinao@student.chalmers.se>
2017-04-25 18:04:59 +0200

    Small adjustments in Question and QuestionActivity

--- app/src/main/java/com/example/nightingale/qwalk/Question.java
+++ app/src/main/java/com/example/nightingale/qwalk/Question.java
@@ -1,69 +1,26 @@
 package com.example.nightingale.qwalk;
 
+import android.media.Image;
+
 /**
  * Created by Elina Olsson on 2017-04-24.
  */
 
 public class Question {
 
-    public Question() {
-
-    }
-
-    private String question;
+    private String questionTitle;
     private String option1;
     private String option2;
     private String option3;
     private String option4;
     private int correctAnswer;
 
-
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getOption1() {
-        return option1;
-    }
-
-    public void setOption1(String option1) {
+    public Question(String title, String option1, String option2, String option3, String option4, int correctAnswer/*Image image, Position position*/ ) {
+        this.questionTitle = title;
         this.option1 = option1;
-    }
-
-    public String getOption2() {
-        return option2;
-    }
-
-    public void setOption2(String option2) {
         this.option2 = option2;
-    }
-
-    public String getOption3() {
-        return option3;
-    }
-
-    public void setOption3(String option3) {
         this.option3 = option3;
-    }
-
-    public String getOption4() {
-        return option4;
-    }
-
-    public void setOption4(String option4) {
         this.option4 = option4;
-    }
-
-    public int getCorrectAnswer() {
-        return correctAnswer;
-    }
-
-    public void setCorrectAnswer(int correctAnswer) {
         this.correctAnswer = correctAnswer;
     }
 
--- app/src/main/java/com/example/nightingale/qwalk/QuestionActivity.java
+++ app/src/main/java/com/example/nightingale/qwalk/QuestionActivity.java
@@ -1,6 +1,5 @@
 package com.example.nightingale.qwalk;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
@@ -16,7 +15,9 @@ import android.widget.Toast;
 
 public class QuestionActivity extends AppCompatActivity {
 
-    TextView questionX;
+    //Välj olika namn på variabler och "ikoner"
+
+    TextView questionNumber;
     EditText question;
     EditText option1;
     EditText option2;
@@ -31,17 +32,25 @@ public class QuestionActivity extends AppCompatActivity {
     RadioButton radioButton3;
     RadioButton radioButton4;
 
+    String questionTitle;
+    String questionOption1;     //Gör detta till en String array istället?
+    String questionOption2;
+    String questionOption3;
+    String questionOption4;
+    String[] questionOptions = {questionOption1, questionOption2, questionOption3, questionOption4};
+    int correctAnswer;
+
     int counter = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question); //ändra namnet till rätt xml-fil
-        question = (EditText) findViewById(R.id.question);
-        option1 = (EditText) findViewById(R.id.option1);
-        option2 = (EditText) findViewById(R.id.option2);
-        option3 = (EditText) findViewById(R.id.option3);
-        option4 = (EditText) findViewById(R.id.option4);
+        question = (EditText) findViewById(R.id.questionField);
+        option1 = (EditText) findViewById(R.id.option1Field);
+        option2 = (EditText) findViewById(R.id.option2Field);
+        option3 = (EditText) findViewById(R.id.option3Field);
+        option4 = (EditText) findViewById(R.id.option4Field);
         number1 = (TextView) findViewById(R.id.number1);
         number2 = (TextView) findViewById(R.id.number2);
         number3 = (TextView) findViewById(R.id.number3);
@@ -51,19 +60,24 @@ public class QuestionActivity extends AppCompatActivity {
         radioButton3 = (RadioButton) findViewById(R.id.radioButton3);
         radioButton4 = (RadioButton) findViewById(R.id.radioButton4);
 
-        questionX = (TextView) findViewById(R.id.questionX);
+        questionNumber = (TextView) findViewById(R.id.questionX);
     }
+
+    //final String questionDefault = question.getText().toString();
+
 
     /*Hur gör jag detta till en generell metod?*/
     public void clearText(View v, EditText textField) {
         textField.getText().clear();
     }
 
-    public void clearQuestion(View view) {
-        question.getText().clear();
+    public void clearQuestion(View view) {      //Clear "this" field. Googla "Find click component"
+        if (question.getText().toString().equals("Skriv fråga här")) {
+            question.getText().clear();
+        }
     }
 
-    public void clearOption1(View view) {
+    public void clearOption1(View view) {   //Kolla upp hur man selectar child för generella metoder
         option1.getText().clear();
         number1.setText("1.");
         number2.setVisibility(View.VISIBLE);
@@ -81,11 +95,28 @@ public class QuestionActivity extends AppCompatActivity {
         radioButton4.setChecked(false);
     }
 
+    /*Hur gör jag detta till en generell metod?*/
+    public int correctAnswer() {
+        if (radioButton1.isChecked()) {
+            return correctAnswer = 0;
+        }
+        else if (radioButton2.isChecked()) {
+            return correctAnswer = 1;
+        }
+        else if (radioButton3.isChecked()) {
+            return correctAnswer = 2;
+        }
+        else if (radioButton4.isChecked()) {
+            return correctAnswer = 3;
+        }
+        return -1;      //Returnerar -1 om inget alternativ är markerat
+    }
+
     /*Ska kolla om användaren har markerat det rätta svaret, spara frågan och gå till nästa fråga*/
     public void addQuestion(View view) {
         if (!(radioButton1.isChecked() || radioButton2.isChecked() || radioButton3.isChecked() || radioButton4.isChecked())) {
             Toast toast = Toast.makeText(getApplicationContext(), "Markera rätt svar", Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 220);
+            toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 220);
             toast.show();
         } else {
             saveQuestion();
@@ -96,7 +127,7 @@ public class QuestionActivity extends AppCompatActivity {
 
     private void newQuestion() {
         counter++;
-        questionX.setText("Fråga " + counter + ".");
+        questionNumber.setText("Fråga " + counter + ".");
         question.getText().clear();
         option1.getText().clear();
         option2.getText().clear();
@@ -106,6 +137,18 @@ public class QuestionActivity extends AppCompatActivity {
 
     /* Denna metod ska spara information till klassen Question */
     private void saveQuestion() {
-
+        //Spara all input till strängar, bild och position
+        //Skapa ny fråga genom att anropa konstruktorn i Question och skicka med titel, alternativ, rätt svar, bild och position
+        questionTitle = question.getText().toString();
+        questionOption1 = option1.getText().toString();
+        questionOption2 = option2.getText().toString();
+        questionOption3 = option3.getText().toString();
+        questionOption4 = option4.getText().toString();
+        correctAnswer = correctAnswer();
+        /*
+        position = ...
+        image = ...
+         */
+        Question question = new Question(questionTitle, questionOption1, questionOption2, questionOption3, questionOption4, correctAnswer);
     }
 }
