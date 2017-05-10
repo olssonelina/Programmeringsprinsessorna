0ae907be76b793d6fa305e4662841aec01dff862
wepia <pia.we94@gmail.com>
2017-05-10 14:58:54 +0200

    MVP ShowResult

--- app/src/main/java/com/example/nightingale/qwalk/View/ShowResultActivity.java
+++ app/src/main/java/com/example/nightingale/qwalk/View/ShowResultActivity.java
@@ -3,13 +3,14 @@ package com.example.nightingale.qwalk.View;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.nightingale.qwalk.InterfaceView.IShowResultActivity;
 import com.example.nightingale.qwalk.R;
 
 /**
  * Created by Elina Olsson on 2017-05-09.
  */
 
-public class ShowResultActivity extends AppCompatActivity {
+public class ShowResultActivity extends AppCompatActivity implements IShowResultActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
