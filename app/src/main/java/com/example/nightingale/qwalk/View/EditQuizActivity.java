60fdeea19ff53254b59e5732541fb6392348745f
Nightingale1997 <kevin.solovjov@gmail.com>
2017-05-23 14:40:57 +0200

    Added EditQuizActivity and EditQuestionActivity

---
+++ app/src/main/java/com/example/nightingale/qwalk/View/EditQuizActivity.java
@@ -0,0 +1,10 @@
+package com.example.nightingale.qwalk.View;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nightingale on 2017-05-23.
+ */
+
+public class EditQuizActivity extends AppCompatActivity {
+}
