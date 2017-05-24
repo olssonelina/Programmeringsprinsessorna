5f1df96a175496e386d64d3957cadf0c94a89766
KraftKatten <kraftj@student.chalmers.se>
2017-05-24 08:57:56 +0200

    Refactored login to MVP

--- app/src/main/java/com/example/nightingale/qwalk/InterfaceView/ILogin.java
+++ app/src/main/java/com/example/nightingale/qwalk/InterfaceView/ILogin.java
@@ -1,8 +1,17 @@
 package com.example.nightingale.qwalk.InterfaceView;
 
+import com.example.nightingale.qwalk.Model.MessageMediator.IOnMessageRecievedListener;
+
 /**
  * Created by Elina Olsson on 2017-05-22.
  */
 
 public interface ILogin {
+    void setSpinnerVisible(boolean value);
+    void openMenu();
+    void openRegister();
+    void enableButtons(boolean value);
+    String getUsername();
+    String getPassword();
+    void showText(String message);
 }
--- app/src/main/java/com/example/nightingale/qwalk/View/LoginActivity.java
+++ app/src/main/java/com/example/nightingale/qwalk/View/LoginActivity.java
@@ -1,205 +1,96 @@
 package com.example.nightingale.qwalk.View;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-
-import com.example.nightingale.qwalk.Model.Database.Account;
-import com.example.nightingale.qwalk.Model.Database.DatabaseHandler;
-import com.example.nightingale.qwalk.Model.MessageMediator.IOnMessageRecievedListener;
+import com.example.nightingale.qwalk.InterfaceView.ILogin;
+import com.example.nightingale.qwalk.Presenter.LoginPresenter;
 import com.example.nightingale.qwalk.R;
 
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-
-public class LoginActivity extends AppCompatActivity implements IOnMessageRecievedListener {
-    String ID;
-    EditText UsernameInput;
-    EditText PasswordInput;
+public class LoginActivity extends AppCompatActivity implements ILogin {
+    private EditText usernameInput;
+    private EditText passwordInput;
     private ProgressBar spinner;
-    Button Loginbutton;
-    Button Registerbutton;
-    Button Guestbutton;
+    private Button loginButton;
+    private Button registerButton;
+    private Button guestButton;
+
+    private LoginPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        UsernameInput = (EditText) findViewById(R.id.username);
-        PasswordInput = (EditText) findViewById(R.id.password);
-        Loginbutton = (Button) findViewById(R.id.LoginButton);
-        Registerbutton = (Button) findViewById(R.id.Registerbutton);
-        Guestbutton = (Button) findViewById(R.id.Guestbutton);
 
+        usernameInput = (EditText) findViewById(R.id.username);
+        passwordInput = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.LoginButton);
+        registerButton = (Button) findViewById(R.id.Registerbutton);
+        guestButton = (Button) findViewById(R.id.Guestbutton);
         spinner = (ProgressBar) findViewById(R.id.progressBar1);
 
-        spinner.setVisibility(View.GONE);
-
-        DatabaseHandler.setOnMessageRecievedListener(this);
-
+        presenter = new LoginPresenter(this);
     }
 
     /**
      * Called when the user taps the Send button
      */
     public void guestButtonClicked(View view) {
-        Account.getInstance().logOut();
-        Intent intent = new Intent(this, MenuActivity.class);
+        presenter.guestButtonPressed();
+    }
+
+    public void registerButtonClicked(View view) {
+        presenter.registerButtonPressed();
+    }
+
+
+    public void loginButtonClicked(View view) {
+        presenter.loginButtonPressed();
+    }
+
+    @Override
+    public void setSpinnerVisible(boolean value) {
+        spinner.setVisibility(value ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void openMenu() {
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
         startActivity(intent);
     }
 
-    public void RegisterButtonClicked(View view) {
+    @Override
+    public void openRegister() {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 
-
-    public void LoginButtonClicked(View view) {
-        Loginbutton.setEnabled(false);
-        Registerbutton.setEnabled(false);
-        Guestbutton.setEnabled(false);
-        spinner.setVisibility(View.VISIBLE);
-
-
-        try {
-            new SendRequest().execute();
-
-
-        } catch (Exception e) {
-
-        }
-
-
-    }
-
-    public class SendRequest extends AsyncTask<String, Void, String> {
-
-        String Username = UsernameInput.getText().toString();
-        String Password = PasswordInput.getText().toString();
-
-        protected void onPreExecute() {
-        }
-
-        protected String doInBackground(String... arg0) {
-
-            try {
-
-                URL url = new URL(DatabaseHandler.VALIDATE_URL);
-
-                JSONObject postDataParams = new JSONObject();
-
-
-                postDataParams.put("username", Username);
-                postDataParams.put("password", Password);
-
-
-                Log.e("params", postDataParams.toString());
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000 /* milliseconds */);
-                conn.setConnectTimeout(15000 /* milliseconds */);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(DatabaseHandler.getPostDataString(postDataParams));
-
-                writer.flush();
-                writer.close();
-                os.close();
-
-                int responseCode = conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-
-                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    StringBuffer sb = new StringBuffer("");
-                    String line = "";
-
-                    while ((line = in.readLine()) != null) {
-
-                        sb.append(line);
-                        break;
-                    }
-
-                    in.close();
-                    return sb.toString();
-
-                } else {
-                    return new String("false : " + responseCode);
-                }
-            } catch (Exception e) {
-                return new String("Exception: " + e.getMessage());
-            }
-        }
-
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            ID = result;
-            ID = ID.replaceAll("\\s+", "");
-            Log.e("CrashID", ID);
-            if (ID.equals("Exception:Unabletoresolvehost\"" + DatabaseHandler.HOST + "\":Noaddressassociatedwithhostname")) {
-
-                Toast.makeText(getApplicationContext(), getResources().getString(R.string.no_internet_ex),
-                        Toast.LENGTH_LONG).show();
-            } else if (ID == null) {
-
-                Toast.makeText(getApplicationContext(), getResources().getString(R.string.connection_failed_ex),
-                        Toast.LENGTH_LONG).show(); //"Connection Failed" -> "Uppkoppnilng misslyckades"
-            } else if (Integer.parseInt(ID) == -1) {
-
-                Toast.makeText(getApplicationContext(), getResources().getString(R.string.incorrect_un_pw_ex),
-                        Toast.LENGTH_LONG).show(); // "Incorrect Password/Username" översatt
-            } else if (Integer.parseInt(ID) == -2) {
-                Account.getInstance().setUserID(Integer.parseInt(ID));
-                Account.getInstance().setUsername(UsernameInput.getText().toString());
-                Toast.makeText(getApplicationContext(), "Admin Success",
-                        Toast.LENGTH_LONG).show();
-                DatabaseHandler.loadFriends();
-
-                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                startActivity(intent);
-            } else {
-                Account.getInstance().setUserID(Integer.parseInt(ID));
-                Account.getInstance().setUsername(UsernameInput.getText().toString());
-                //Toast.makeText(getApplicationContext(), getResources().getString(R.string.done),
-                //Toast.LENGTH_LONG).show(); // "Success" -> "Klar"
-                DatabaseHandler.loadFriends();
-                spinner.setVisibility(View.GONE);
-                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                startActivity(intent);
-            }
-            spinner.setVisibility(View.GONE);
-            Loginbutton.setEnabled(true);
-            Registerbutton.setEnabled(true);
-            Guestbutton.setEnabled(true);
-        }
+    @Override
+    public void enableButtons(boolean value) {
+        loginButton.setEnabled(value);
+        registerButton.setEnabled(value);
+        guestButton.setEnabled(value);
     }
 
     @Override
-    public void messageRecieved(String message) {
+    public String getUsername() {
+        return usernameInput.getText().toString();
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordInput.getText().toString();
+    }
+
+    @Override
+    public void showText(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
