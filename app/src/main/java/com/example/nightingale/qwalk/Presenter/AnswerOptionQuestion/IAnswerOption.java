0a987de24602e1a740497caebca5e61161fcd5e0
KraftKatten <kraftj@student.chalmers.se>
2017-05-28 23:59:41 +0200

    Added comments

--- app/src/main/java/com/example/nightingale/qwalk/Presenter/AnswerOptionQuestion/IAnswerOption.java
+++ app/src/main/java/com/example/nightingale/qwalk/Presenter/AnswerOptionQuestion/IAnswerOption.java
@@ -7,8 +7,16 @@ import com.example.nightingale.qwalk.Model.Question.OptionQuestion;
  */
 
 public interface IAnswerOption {
+    /**
+     * @param options the different answers to choose from
+     */
     void setOptions(String[] options);
 
+    /**
+     * Closes the view, and sends the users answer to a specific question to another view.
+     * @param chosenIndex the players answer
+     * @param question the question that was answered
+     */
     void closeWithResult(int chosenIndex, OptionQuestion question);
 
     void setOptionColour(int index, boolean isSelectedColour);
